1980eecd06f27236a92036c6b8e9a85fedc0cb4c
MohammadMaasir <maasir554@gmail.com>
2024-09-26 03:49:11 +0530

    font issue solved

--- app/components/Subscribe.jsx
+++ app/components/Subscribe.jsx
@@ -1,7 +1,7 @@
 export const Subscribe = ({className=""}) => (
     <div className={"flex w-full rounded-3xl bg-white py-14 sm:py-16 md:py-20 px-10 sm:px-12 md:px-16 shadow-xl mt-20 items-end justify-center gap-10 flex-wrap " + className}>
         <span className="flex flex-col gap-4 ">
-            <h3 className=" sm:text-lg md:text-xl lg:text-2xl text-secondary font-caveat">
+            <h3 className=" text-2xl lg:text-3xl text-secondary font-caveat">
                 Don't Miss our updates
             </h3>
 
@@ -10,7 +10,7 @@ export const Subscribe = ({className=""}) => (
             </h1>
         
         </span>
-        <span className="lg:flex-1 rounded-none p-5 sm:p-0 sm:rounded-full shadow-lg flex bg-neutral-100 overflow-hidden h-auto sm:h-14 md:h-16 md:min-w-[400px] w-full flex-wrap sm:flex-nowrap justify-center sm:justify-start gap-4 sm:gap-0" >  
+        <span className="lg:flex-1 rounded-none p-5 sm:p-0 sm:rounded-full shadow-lg flex bg-neutral-100 overflow-hidden h-auto sm:h-14 md:h-16 md:min-w-[400px] w-full flex-wrap sm:flex-nowrap justify-center sm:justify-start gap-4 sm:gap-0 max-w-7xl" >  
             <input type="email" placeholder="Enter your email..." className="bg-transparent pl-5 md:pl-10 w-full p-4 focus:outline-none text-xs sm:text-sm md:text-md " />
             <button className=" h-auto sm:h-full flex items-center justify-center py-3 px-8 md:px-14 text-white font-semibold bg-gradient-to-r from-secondary to-primary rounded-full hover:bg-gradient-to-r hover:from-primary hover:to-secondary text-sm sm:text-lg"> 
                 Subscribe
